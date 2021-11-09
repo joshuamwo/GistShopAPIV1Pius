b8a74ce55a724cb552235f0893411c7d7e574599
PiusKariuki <piuskariuki19@gmail.com>
2021-11-09 13:51:07 +0300

    fixed task routes

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,16 +2,19 @@ const express = require("express");
 var taskRouter = express.Router();
 var taskController = require("../controllers/tasks");
 
-taskRouter.route(`/`).post(taskController.postTask);
+taskRouter
+    .route("/")
+    .post(taskController.postTask);
 
-taskRouter.route(`/departments/:department`).get(taskController.getTasks);
+taskRouter
+  .route(`/departments/:department`)
+  .get(taskController.getTasks);
+  
 
 taskRouter
   .route("/:taskId")
   .get(taskController.getTaskById)
-
   .put(taskController.editTask)
-
   .delete(taskController.deleteTask);
 
 module.exports = taskRouter;
