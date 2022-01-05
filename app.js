3d6e771dcc6224508c0cc5ff859c85d500c5675e
PiusKariuki <piuskariuki19@gmail.com>
2022-01-05 16:05:00 +0300

    hey

--- app.js
+++ app.js
@@ -5,30 +5,27 @@ require("dotenv").config({ path: `${__dirname}/CONFIG/.env` });
 require("./Services/authenticate");
 
 /*****************
-  *SERVER INITILIZATIONS
-*****************/
+ *SERVER INITILIZATIONS
+ *****************/
 const app = express();
 
-
-
 /*****************
-  *VIEW ENGINE CONFIG
-*****************/
+ *VIEW ENGINE CONFIG
+ *****************/
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 /*****************
-  *MIDDLEWARE
-*****************/
+ *MIDDLEWARE
+ *****************/
 app.use(require("./Services/middleware"));
 app.use(require("./Routes/ROUTE_MOUNTER"));
 
 /*****************
-  *SERVER INSTANTIATION
-*****************/
+ *SERVER INSTANTIATION
+ *****************/
 const port = process.env.PORT || 5000;
 connect();
 app.listen(port);
-
 
 module.exports = app;
