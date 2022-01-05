59eaa4a26f79c227ca81ff9fd28e0b831cc2f1f5
PiusKariuki <piuskariuki19@gmail.com>
2022-01-05 19:07:03 +0300

    vercel deploy

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,6 +8,6 @@ authRouter.post("/login", authController.userLogin);
 
 authRouter.post("/admin/login", authController.adminLogin);
 
-authRouter.get("/logout", authController.logout);
+authRouter.post("/logout", authController.logout);
 
 module.exports = authRouter;
