f1a78b968f4646ecdeeb5cc906cfe7d39e1112f4
PiusKariuki <piuskariuki19@gmail.com>
2022-01-22 11:44:24 +0300

    destructuring errors in register

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -30,7 +30,10 @@ exports.register = async (req, res, next) => {
 	} catch (error) {
 		res.status(422).setHeader("Content-Type", "application/json");
 		if (error.code === 11000) res.json("This user already exists");
-		else res.json({ error: { errors } });
+		else {
+			const { errors } = error;
+			res.json(errors);
+		}
 	}
 };
 
