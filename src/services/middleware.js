953bed84cb35a68206f28f710f4b032d8bdece4a
PiusKariuki <piuskariuki19@gmail.com>
2022-02-17 15:05:38 +0300

    disabled helmet middleware

--- src/services/middleware.js
+++ src/services/middleware.js
@@ -27,4 +27,4 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-app.use(helmet({crossOriginEmbedderPolicy: false,}));
+// app.use(helmet({crossOriginEmbedderPolicy: false,}));
